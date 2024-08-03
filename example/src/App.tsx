030c27ad0f0a852ed67bc70ad3747fa7843880aa
Kerim Can Tunç <kerimcan.tunc@fibabanka.com.tr>
2024-08-03 01:21:48 -0400

    deneme

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,10 @@
 import { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'youtube-search-fetch';
 
 export default function App() {
   const [result, setResult] = useState<number | undefined>();
 
-  useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <View style={styles.container}>
